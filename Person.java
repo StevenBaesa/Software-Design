8e61377e97809a0a5bc8f51534faeef0c72c8ce7
StevenBaesa <79894327+StevenBaesa@users.noreply.github.com>
2021-03-08 18:12:06 +0800

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,10 @@
 public class Name
 {
-public string firstname = "Steven Ryan";
-public string lastname = "Baesa";
+public static void main(String[] args) {
+  String fname = new String("Steven Ryan");
+  String lname = new String("Baesa);
+                           
+  System.out.println(fname);
+  System.out.println(lname);
+          }
+     }
