aa0ca39999758aca887e25ea516d84b1071235f4
StevenBaesa <79894327+StevenBaesa@users.noreply.github.com>
2021-03-08 18:28:40 +0800

    Update Person.java
    Modify

--- Person.java
+++ Person.java
@@ -8,3 +8,23 @@ public static void main(String[] args) {
   System.out.println(lname);
           }
      }
+public class Name
+ {
+ private string fname;
+ public string getfname();
+   return this.fname;
+ }
+   public void set fname(string fname) {
+     this.fname = fname;
+          }
+     }
+ public class Name
+ {
+   private string lname;
+   public string getlname();
+    return this.lname;
+ }
+   public void set lname(string lname) {
+     this.lname = lname;
+        }
+   }
