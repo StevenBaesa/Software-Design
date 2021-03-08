0c6fafd9985ab56704506d84c5115e993cfb0f69
StevenBaesa <79894327+StevenBaesa@users.noreply.github.com>
2021-03-08 19:30:14 +0800

    Create Main.java
    Final

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+  
+  public static void main (String[] args) {
+    String fname1 = new String("Steven");
+    String lname1 = new String("Baesa");
+    String fname2 = new String("Garen");
+    String lname2 = new String("Crownguard");
+    String fname3 = new String("Naruto");
+    String lname3 = new String("Uzumaki");
+    String fname4 = new String("Sasuke");
+    String lname4 = new String("Uchiha");
+    String fname5 = new String("Sakura");
+    String lname5 = new String("Haruno");
+    
+    System.out.println(fname1);
+    System.out.println(lname1);
+    System.out.println(fname2);
+    System.out.println(lname2);
+    System.out.println(fname3);
+    System.out.println(lname3);
+    System.out.println(fname4);
+    System.out.println(lname4);
+    System.out.println(fname5);
+    System.out.println(lname5);
+  }
+}
